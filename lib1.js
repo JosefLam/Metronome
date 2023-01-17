1cff466a71adadd83e8846bcd8d905d6e38e4c66
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2023-01-17 19:56:42 +0000

    Restored

--- lib1.js
+++ lib1.js
@@ -1,23 +1,66 @@
-// timeser
-console.log("Setting up timeser");
-const txt1 = document.getElementById("txt1");
-const txt2 = document.getElementById("txt2");
-var answer = 0;
+// metronome
+console.log("Setting up metronome");
+const btnDecreaseBPM = document.getElementById("btnDecreaseBPM");
+const btnIncreaseBPM = document.getElementById("btnIncreaseBPM");
+const bpmInput = document.getElementById("txtBPM");
+const btnStartStop = document.getElementById("btnStartStop");
+const MAX_BPM = 120
+const MIN_BPM = 50
+var running = false;
+var bpm = 60;   //beats per minute
 
-txt1.onchange = function (e) {
-    num1 = parseInt(txt1.value);
-    num2 = parseInt(txt2.value);
-    answer = num1 * num2;
-    console.log(answer)
-    document.getElementById("display").value = parseStr(answer.value);
-    
+btnStartStop.addEventListener("click", function (e) {
+    console.log("click");
+    running = !running;
+    btnStartStop.value = running ? "Stop" : "Start";
+
+
+    if (running) {
+        setSpeedAndStart();
+    }
+});
+
+function setSpeedAndStart() {
+    clearInterval(callback)
+    var ms = 60000 / bpm;
+    callback = setInterval(onTick, 1000);
 }
 
-txt2.onchange = function (e) {
-    num1 = parseInt(txt1.value);
-    num2 = parseInt(txt2.value);
-    answer = num1 * num2;
-    console.log(answer)
-    document.getElementById("display").value = parseStr(answer.value);
+function onTick() {
+    console.log("Tick");
+}
 
+bpmInput.onchange = function (e) {
+    bpm = parseInt(bpmInput.value);
+    if (isNan(bpm)) {
+        bpm = 50;
+    }
+    console.log(bpm);
+}
+
+
+btnDecreaseBPM.onclick = function (e) {
+    bpm -= 5;
+    console.log("Decrease BPM");
+    updateBPM();
+}
+
+btnIncreaseBPM.onclick = function (e) {
+    bpm += 5;
+    console.log("Increase BPM");
+    updateBPM();
+}
+
+function updateBPM() {
+    if (bpm > MAX_BPM) {
+        bpm = MAX_BPM;
+    }
+    if (bpm < MIN_BPM) {
+        bpm = MIN_BPM;
+    }
+
+    bpmInput.value = bpm;
+    if (running) {
+        setSpeedAndStart();
+    }
 }
